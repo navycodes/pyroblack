3e0a92e16948a16e487922eef42abc6fe897eb86
navycodes <149182882+navycodes@users.noreply.github.com>
2025-01-05 09:35:06 +0700

    add keyboard

--- pyrogram/helpers/__init__.py
+++ pyrogram/helpers/__init__.py
@@ -15,3 +15,4 @@ along with pyromod.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 from .helpers import ikb, bki, ntb, btn, kb, kbtn, array_chunk, force_reply
+from .keyboard import (InlineKeyboard, InlineButton, InlinePaginationKeyboard, InlineButton, ReplyKeyboard, ReplyButton, ReplyKeyboardRemove, ForceReply)
